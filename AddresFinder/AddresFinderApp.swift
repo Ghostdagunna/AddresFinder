31be76d87149598b173b3065ffdbec0f02c31c48
Jordan Mck <ghostdagunna@gmail.com>
2023-10-04 11:12:44 -0500

    Initial Commit

---
+++ AddresFinder/AddresFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddresFinderApp.swift
+//  AddresFinder
+//
+//  Created by Jordan Mck on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AddresFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
